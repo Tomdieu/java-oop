f3e328d9da61627fb81ea319df4e73766c145819
Tomdieu <ivantomdio@gmail.com>
2022-11-01 20:25:37 +0100

    Exo2

---
+++ Exo2/src/com/exo2/Test.java
@@ -0,0 +1,9 @@
+package com.exo2;
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Pointcol p = new Pointcol(3,5,(byte)25);
+        p.colorer();
+        // p.afficher();
+        p.afficheCol();
+    }
+}
